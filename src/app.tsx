9029233e2cac168da83871e5ec3a2513f0f3ddd5
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-11-17 14:11:57 +0100

    ad  backend for storage contacts

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "./store";
-import { addContact, deleteContact, setFilter } from "./contactsSlice";
-import { nanoid } from "nanoid";
+import { RootState, AppDispatch } from "./store";
+import {
+  fetchContacts,
+  addContact,
+  deleteContact,
+  setFilter,
+} from "./contactsSlice";
 import { ContactForm, Filter, ContactList } from "./components";
 import styles from "./app.module.css";
 
 const App: React.FC = () => {
-  const contacts = useSelector((state: RootState) => state.contacts.contacts);
-  const filter = useSelector((state: RootState) => state.contacts.filter);
-  const dispatch = useDispatch();
+  const {
+    items: contacts,
+    isLoading,
+    error,
+    filter,
+  } = useSelector((state: RootState) => state.contacts);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   const handleAddContact = (name: string, number: string) => {
     if (contacts.some((contact) => contact.name === name)) {
       alert(`${name} is already in contacts.`);
       return;
     }
-    const newContact = { id: nanoid(), name, number };
-    dispatch(addContact(newContact));
+    dispatch(addContact({ name, number }));
   };
 
   const handleDeleteContact = (id: string) => {
@@ -38,7 +50,16 @@ const App: React.FC = () => {
       <ContactForm onSubmit={handleAddContact} />
       <h2 className={styles.h2}>Contacts</h2>
       <Filter filter={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={handleDeleteContact}
+        />
+      )}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import { Provider } from "react-redux";
-import { store, persistor } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("app")!);
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>
 );
